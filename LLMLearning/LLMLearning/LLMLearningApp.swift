cdd303bf718e9cab376dfec711a7a382a3bf6331
miltonpl <miltonpalaguachi@gmail.com>
2026-06-27 13:03:45 -0400

    Xcode setup

---
+++ LLMLearning/LLMLearning/LLMLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LLMLearningApp.swift
+//  LLMLearning
+//
+//  Created by Milton Palaguachi on 6/27/26.
+//
+
+import SwiftUI
+
+@main
+struct LLMLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LLMLearning/LLMLearningUITests/LLMLearningUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  LLMLearningUITestsLaunchTests.swift
+//  LLMLearningUITests
+//
+//  Created by Milton Palaguachi on 6/27/26.
+//
+
+import XCTest
+
+final class LLMLearningUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
